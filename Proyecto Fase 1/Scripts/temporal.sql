538da9bb730e927288e18b85eddcb45e430015dc
Kevin <3664028620101@ingenieria.usac.edu.gt>
2024-03-10 12:39:55 -0600

    Fase1

---
+++ Proyecto Fase 1/Scripts/temporal.sql
@@ -0,0 +1,43 @@
+CREATE TABLE CompraTemp (
+    fecha varchar(50),
+    codProveedor varchar(50),
+    nombreProveedor varchar(100),
+    direccionProveedor varchar(150),
+    numeroProveedor varchar(50),
+    webProveedor varchar(100),
+    codProducto varchar(50),
+    nombreProducto varchar(100),
+    marcaProducto varchar(100),
+    categoria varchar(100),
+    sodSucursal varchar(50),
+    nombreSucursal varchar(100),
+    direccionSucursal varchar(150),
+    region varchar(50),
+    departamento varchar(50),
+    unidades varchar(50),
+    costoU varchar(50)
+);
+
+
+CREATE TABLE ventaTemp (
+    fecha varchar(50),
+    codCliente varchar(50),
+    nombreCliente varchar(100),
+    tipoCliente varchar(50),
+    direccionCliente varchar(150),
+    numeroCliente varchar(50),
+    codVendedor varchar(50),
+    nombreVendedor varchar(100),
+    vacacionista varchar(50),
+    codProducto varchar(50),
+    nombreProducto varchar(100),
+    marcaProducto varchar(100),
+    categoria varchar(100),
+    sodSucursal varchar(50),
+    nombreSucursal varchar(100),
+    direccionSucursal varchar(150),
+    region varchar(50),
+    departamento varchar(50),
+    unidades varchar(50),
+    precioUnitario varchar(50)
+);
